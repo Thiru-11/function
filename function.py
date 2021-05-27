2fadc533554e4fe78fc7ee447a9e79a3658f684b
Thiru-11 <79251410+Thiru-11@users.noreply.github.com>
2021-05-27 21:45:48 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,10 @@
+most_frequent = "assasinscreed"
+all_freq = {}
+
+for i in most_frequent:
+	if i in all_freq:
+		all_freq[i] += 1
+	else:
+		all_freq[i] = 1
+
+print ("Count of all characters in GeeksforGeeks is :\n "
